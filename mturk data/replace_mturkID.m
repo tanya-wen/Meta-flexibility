f90252644e6a63068ca726c27b9359d9e1cbf6b3
tanya-wen <56415589+tanya-wen@users.noreply.github.com>
2021-07-18 23:24:27 -0400

    remove demographics

--- mturk data/replace_mturkID.m
+++ mturk data/replace_mturkID.m
@@ -12,6 +12,16 @@ for subj = 1:nsubj
     data = readtable(files(subj).name,'FileType','text');
     data.workerId = data.subject;
     
+    % remove demographic info
+    % (age, race, gender, ethnicity, colorbindness, normal vision, neurological disorder)
+    try
+        fields = {'age','race','gender','ethnicity','color','vision','neuro'};
+        data = removevars(data,fields);
+        data(end,:) = []; % last row is demographics response
+    catch
+    end
+    
+    % write out data
     writetable(data,fullfile(files(subj).folder,files(subj).name),'FileType','text');
     
 end
